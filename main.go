56db214b495f81850bac12d848cc1fd3fb0e4910
pharanyuwave@gmail.com <pharanyuwave@gmail.com>
2023-10-26 09:24:11 +0700

    feature: display

--- main.go
+++ main.go
@@ -14,11 +14,14 @@ func mul(a, b int) int {
 func div(a, b float64) float64 {
 	return a / b
 }
-
+func display(msg string) {
+	fmt.Println(msg)
+}
 func main() {
 	fmt.Println("First Commit")
 	fmt.Println(sum(1, 2))
 	fmt.Println(sub(1, 2))
 	fmt.Println(mul(1, 2))
 	fmt.Println(div(1, 2))
+	display("Hello")
 }
